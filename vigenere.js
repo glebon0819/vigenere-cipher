81f41c6712f30a20c920340ceff5b37fb5803d26
glebon0819 <g.lebon0819@gmail.com>
2017-07-18 21:29:14 -0700

    Fixed key non-alpha characters
    Now, any non-alphabetical characters found in the text will be inserted
    into the key in order to properly space out the key characters.

--- vigenere.js
+++ vigenere.js
@@ -21,49 +21,62 @@ function vigenereCipher(text, key) {
         }
     });
     this.encrypt = function() {
-        var text_length = text.length;
-        var key_length = key.length;
-        if (text_length > 0 && key_length > 0) {
-            // make key have same length as text
-            var keyConst = key;
-            while (text_length !== key_length) {
-                if (text_length > key_length) {
-                    // lengthen the key
-                    key += keyConst;
-                } else if (text_length < key_length) {
-                    // shorten the key (is this really necessary?)
-                    var difference = key_length - text_length;
-                    key = key.substring(0, key_length - difference);
-                }
-                key_length = key.length;
-            }
-            console.log(text);
-            console.log(key);
-            console.log(text_length);
-            var result = "";
-            // go through each A-z character in the text, skipping anything else
-            var abs = text_length;
-            for (var i = 0; i < abs; i++) {
-                // for the current character index, get the key value at that index
-                // get the UTF code of the current key character
-                var code = key.charCodeAt(i);
-                if (code >= 97 && code <= 122) {
-                    code = code - 97;
-                } else if (code >= 65 && code <= 90) {
-                    code = code - 65;
-                }
-                const caesar = new caesarCipher(text.charAt(i), code);
-                var encoded = caesar.encrypt();
-                result += encoded;
-            }
-            return result;
-            // clean that code up so that it fits with the 0-25 model
-            // use the current text character as the text and the cleaned code as the offset for a Caesar cipher
-            // add returned cipher to the result string
-            // return result string
-        } else {
-            console.log("Text or key or both are not set.");
-            return false;
-        }
+		var key = this.key;
+		var text = this.text;
+		key = key.replace(/[^a-z]/gi, '');
+		var text_length = this.text.length;
+		var key_length = this.key.length;
+		if (text_length > 0 && key_length > 0) {
+			// make key have same length as text
+			console.log(key);
+			var keyConst = key;
+			while (text_length > key_length) {
+				key += keyConst;
+				key_length = key.length;
+			}
+			for(var i = 0; i < text.length; i++){
+				var current_text = text.charAt(i);
+				if(!current_text.match(/[a-z]/i)){
+				// splice the current character into the key at [i]
+					key = key.slice(0, i) + current_text + key.slice(i);
+				}
+			}
+			if(text_length < key_length){
+				var difference = key_length - text_length;
+				key = key.substring(0, key_length - difference);
+			}
+					
+			console.log(text);
+			console.log(key);
+			//console.log(text_length);
+			var result = "";
+			// go through each A-z character in the text, skipping anything else
+			var abs = text_length;
+			for(var i = 0; i < abs; i++) {
+				// for the current character index, get the key value at that index
+				// get the UTF code of the current key character
+				var code = key.charCodeAt(i);
+				if (code >= 97 && code <= 122) {
+					code = code - 97;
+				} else if (code >= 65 && code <= 90) {
+					code = code - 65;
+				}
+				const caesar = new caesarCipher(text.charAt(i), code);
+				var encoded = caesar.encrypt();
+				if(encoded){
+					result += encoded;
+				} else {
+					result += text.charAt(i);
+				}
+			}
+		return result;
+		// clean that code up so that it fits with the 0-25 model
+		// use the current text character as the text and the cleaned code as the offset for a Caesar cipher
+		// add returned cipher to the result string
+		// return result string
+		} else {
+			console.log("Text or key or both are not set.");
+			return false;
+		}
     }
 }
